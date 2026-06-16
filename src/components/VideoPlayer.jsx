20835d882807bada5645c23398e25cd9b82f0d5e
Monol1tovec <92056083+Monol1tovec@users.noreply.github.com>
2026-06-17 01:03:23 +0300

    Update VideoPlayer.jsx

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -4,13 +4,18 @@ import './VideoPlayer.css';
 const VideoPlayer = forwardRef((props, ref) => {
   return (
     <section className="video-section fade-in" ref={ref}>
-      <div className="video-container">
-        <iframe
-          src="https://drive.google.com/file/d/1iLQPqt_pYQHp1FwoNkUZYPS8UJyQvtDn/preview"
-          allow="autoplay; fullscreen"
-          allowFullScreen
-          title="THE BETARTEM Film"
-        />
+      <div className="netflix-player">
+
+        <video
+          className="video"
+          controls
+          playsInline
+          preload="metadata"
+        >
+          <source src="/the-betartem-film/movie.mp4" type="video/mp4" />
+          Ваш браузер не поддерживает видео.
+        </video>
+
       </div>
     </section>
   );
